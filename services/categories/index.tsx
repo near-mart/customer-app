a5e2576c81a32c6083d62d0df83276ff02dc9816
TusharGola28 <tushar.gola@ticketsque.com>
2025-10-23 21:48:13 +0530

    first commit

--- services/categories/index.tsx
+++ services/categories/index.tsx
@@ -1,6 +1,6 @@
 import { api } from "@/lib/AxiosInstants"
 
 export const fetchCategory = async (signal: any,) => {
-    const response = await api.get("/service/catalogue_service/v1/web/category", { signal })
+    const response = await api.get("/service/catalogue_service/v1/no-auth/web/category", { signal })
     return response.data
 }
--- services/suppliers/index.tsx
+++ services/suppliers/index.tsx
@@ -2,7 +2,7 @@ import { api } from "@/lib/AxiosInstants";
 
 export const fetchSupplier = async (signal: any, params: { page?: number, page_size?: number, search?: string, supplier?: string, location?: any }) => {
     try {
-        const response = await api.get("/service/customer_service/v1/web/suppliers", { signal, params: { ...params } })
+        const response = await api.get("/service/customer_service/v1/no-auth/web/suppliers", { signal, params: { ...params } })
         return response.data
     } catch (err: any) {
         console.log(err);
