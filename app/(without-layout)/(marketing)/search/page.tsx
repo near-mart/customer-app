3b3f06d4bc25ba5695975f373d9b5c771e0afd64
TusharGola28 <tushar.gola@ticketsque.com>
2025-10-30 00:17:46 +0530

    add new features

--- app/(without-layout)/(marketing)/search/page.tsx
+++ app/(without-layout)/(marketing)/search/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from 'react'
 import SearchMain from './_components'
 
 export default function Search() {
     return (
         <div>
-            <SearchMain />
+            <Suspense fallback={<div className="p-10 text-center">Loading search...</div>}>
+                <SearchMain />
+            </Suspense>
         </div>
     )
 }
